d1edf6115347b3b7363e2c79df134e9381cd1f2e
Matt Chauta <mchauta@nrao.edu>
2019-06-19 16:06:48 -0400

    adding custom post types

--- functions.php
+++ functions.php
@@ -1,1 +1,42 @@
 <?php
+
+function create_reviews() {
+  register_post_type( 'reviews',
+    array(
+      'labels' => array(
+        'name' => __( 'Reviews' ),
+        'singular_name' => __( 'Review' ),
+        'edit_item' => __( 'Edit Review' ),
+        'new_item' => __( 'New Review' )
+      ),
+      'public' => false,
+      'has_archive' => true,
+      'rewrite' => array('slug' => 'reviews'),
+      'menu_icon'   => 'dashicons-star-empty',
+			'show_in_nav_menus' => true,
+    	'show_ui' => true,
+    )
+  );
+}
+add_action( 'init', 'create_reviews' );
+
+function create_services() {
+  register_post_type( 'services',
+    array(
+      'labels' => array(
+        'name' => __( 'Services' ),
+        'singular_name' => __( 'Service' ),
+        'edit_item' => __( 'Edit Service' ),
+        'new_item' => __( 'New Service' )
+      ),
+      'public' => false,
+      'has_archive' => false,
+      'taxonomies'  => array( 'category' ),
+      'supports' => array('title', 'editor','thumbnail', 'revisions', 'page-attributes'),
+      'menu_icon'   => 'dashicons-tickets-alt',
+			'show_in_nav_menus' => true,
+    	'show_ui' => true,
+    )
+  );
+}
+add_action( 'init', 'create_services' );
