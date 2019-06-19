dea95fa135976ad73fccdc10c78cdb7ad70d53dd
Matt Chauta <mchauta@nrao.edu>
2019-06-19 17:01:36 -0400

    bugs

--- page-reviews.php
+++ page-reviews.php
@@ -30,8 +30,8 @@ get_header(); ?>
 											<div class="rev-img"><?php the_post_thumbnail(); ?></div>
 										</div>
 										<div class="col-md-8 col-sm-12">
-											<?php the_content(); ?>
-											<div class="rev-name"><?php echo $reviewer; ?></div>
+											<div class="rev-rev">"<?php the_content(); ?>"</div>
+											<div class="rev-name">-<?php echo $reviewer; ?></div>
 										</div>
 									</div>
 								</div>
